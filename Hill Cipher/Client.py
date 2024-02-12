284d03cb6764a5b10f202382601af56679beb4a5
Syed Sahil <120191200+SyedSahil80328@users.noreply.github.com>
2024-02-12 19:31:35 +0530

    Update Client.py

--- Hill Cipher/Client.py
+++ Hill Cipher/Client.py
@@ -13,30 +13,37 @@ def startClient():
     host = '127.0.0.1'
     port = 12345
     clientSocket.connect((host, port))
-    print(f"Connected to server at {host}:{port}")
+    print(f"\nConnected to server at {host}:{port}")
 
     # Send data to the server
     plainText = input("Enter a message to send to the server: ").upper()
+    if plainText == "EXIT":
+        clientSocket.sendall(plainText.encode('utf-8'))
+        clientSocket.close()
+        return 0
+
     keyLengths = findFactors(len(plainText))
     print("\n\tWARNINGS BEFORE ENTERING A KEY")
-    print("The program will terminate if:")
+    print("You will be Reprompted to enter a key if:")
     print(f"\t1. The length of key is not in {keyLengths}")
     print("\t2. It's absolute determinant is an even number (even 0)")
     print("\t3. It's absolute determinant is a multiple of 13.\nSo enter it responsibly.\n")
+    
     key = input("Enter a key: ").upper()
-
     keyLength = len(key)
-
-    if keyLength not in keyLengths:
-        print("Keylength is inappropriate. So encryption fails.")
-        exit(0)
-
     keyMatrix = convertKeyToMatrix(key)
     det = abs(determinant(keyMatrix))
 
-    if det % 2 == 0 or det % 13 == 0:
-        print(f"The gcd({det}, 26) is > 1. So modular multiplicative inverse never exists. So encryption will succeed, but decryption?")
-        exit(0)
+    while keyLength not in keyLengths or (det%2 == 0 or det%13 == 0):
+        if keyLength not in keyLengths:
+            print("Keylength is inappropriate. So encryption fails.")
+        if det % 2 == 0 or det % 13 == 0:
+            print(f"The gcd({det}, 26) is > 1. So modular multiplicative inverse never exists. So encryption will succeed, but decryption?")
+
+        key = input("Enter a key: ").upper()
+        keyLength = len(key)
+        keyMatrix = convertKeyToMatrix(key)
+        det = abs(determinant(keyMatrix))
 
     cipherText = encode(plainText, keyMatrix)
     keyMatrixData = json.dumps(keyMatrix)
@@ -50,6 +57,11 @@ def startClient():
 
     # Close the connection
     clientSocket.close()
+    return 1
 
 if __name__ == "__main__":
-    startClient()
+    i = 0
+    while startClient():
+        i += 1
+    
+    print("Program Terminated.")
